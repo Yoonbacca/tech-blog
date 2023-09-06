7b896846b6bed07aa1bba56c10ae0b2afe157aa4
YoonJae Kim <yoonjaekim3@gmail.com>
2023-09-06 13:15:37 -0500

    Refactored blog model to a post model instead

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newBlog = await Blog.create({
+    const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newBlog);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,12 +17,12 @@ router.post('/', withAuth, async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const blogData = await Blog.findByPk(req.params.id);
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
+    const postData = await Post.findByPk(req.params.id);
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
-    res.status(200).json(blogData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -30,19 +30,19 @@ router.get('/:id', async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const blogData = await Blog.destroy({
+    const postData = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
-    res.status(200).json(blogData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
