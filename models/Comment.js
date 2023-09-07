86ee6df1ea52480d41f86cf0a8d87ba4a4733117
YoonJae Kim <yoonjaekim3@gmail.com>
2023-09-07 14:33:11 -0500

    Created comment model

--- models/Comment.js
+++ models/Comment.js
@@ -25,12 +25,12 @@ Comment.init(
         model: 'user',
         key: 'id',
       },
-      post_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'post',
-          key: 'id',
-        },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
     },
   },
   {
